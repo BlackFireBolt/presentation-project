2de91645276fc3d448397b070afb045881c233df
aboltikov <andrey.boltikov@camlingroup.com>
2024-10-07 11:53:00 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/presentation-project/'
+  base: '/presentation-project/',
   plugins: [
     vue(),
   ],
